a2eeeab13e670c1636d6303969f930eb02983db7
solkiona <solkiona@gmail.com>
2023-02-16 17:55:03 -0800

    betty you are really soemthign

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
  * main is where my function begin
+ * Return is the value it returns
  */
-  int main(void)
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
