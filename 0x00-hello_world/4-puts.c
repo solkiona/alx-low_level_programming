737b1c33c709dae11619e4d687bfdae872149a03
solkiona <solkiona@gmail.com>
2023-02-16 17:50:38 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main is where my function begins 
- * returns 0 since the type is integer
-  */
-int main(void)
+ * main is where my function begin
+ */
+ 
+ int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 		return (0);
