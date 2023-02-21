8af53f5b69f1fefd15adc7dc9e3e500036f1f019
solkiona <solkiona@gmail.com>
2023-02-21 15:54:52 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
  */
 void print_alphabet(void)
 {
-    char c = 'a';
-    while (c <= 'z')
+    int i = 97;
+    while (i <= 122)
     {
-     _putchar(c);
-     c++;
+     _putchar(i);
+     i++;
     }
     _putchar("\n");
 }
