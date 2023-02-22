d7e72e47a5b44c8c8c85d21dc09d84e33ba01f6e
solkiona <solkiona@gmail.com>
2023-02-21 20:26:22 -0800

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,21 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 0.
-* on error, -1 is returned, and errno is set appropriately.
-*/
-int _abs(int c)
+ * _abs - Computes the absolute value
+ * of an integer.
+ *
+ * @i: input number as an integer.
+ *
+ * Return: absolute value
+ */
+int _abs(int i)
 {
-	if (c < 0)
+	if (i >= 0)
 	{
-	c *= -1;
-	return c;
+		return (i);
 	}
 	else
 	{
-	return c;
+		return (i * -1);
 	}
 }
