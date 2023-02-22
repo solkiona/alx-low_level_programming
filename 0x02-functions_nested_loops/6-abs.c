9131365ca27ba0d24571d8128da2d081b6ede143
solkiona <solkiona@gmail.com>
2023-02-21 20:22:04 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 0.
+* on error, -1 is returned, and errno is set appropriately.
+*/
+int _abs(int c)
+{
+	if (c < 48)
+	{
+	c *= -1;
+	return c;
+	}
+	else
+	{
+	return c;
+	}
+}
