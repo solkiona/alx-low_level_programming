d6c306a4a88646fb54e159202d902345509c4f76
solkiona <solkiona@gmail.com>
2023-02-21 19:48:54 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <ctype.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+    if (c >= 'a' && c<= 'z')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
