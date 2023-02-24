ec64868059e882127f57f08498bb7267a4c6401f
solkiona <solkiona@gmail.com>
2023-02-24 08:11:34 -0800

    done1

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,9 +2,11 @@
 #include "main.h"
 
 /**
- * _putchar - writes result to the console
- * @c: input character
- * Return: prints result to console
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar (char c)
 {
