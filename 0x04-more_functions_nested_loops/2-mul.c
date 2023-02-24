752e08d784126597b590349ca0d94a4ddf7f61fe
solkiona <solkiona@gmail.com>
2023-02-24 00:58:51 -0800

    multplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int mul - function that multiplies two digits
+ * @a: input 1
+ * @b: input 2
+ * Return: Returns the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
