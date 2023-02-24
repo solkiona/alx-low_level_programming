7087e05d1164e29f0334d4541c1e37ba39777752
solkiona <solkiona@gmail.com>
2023-02-24 01:33:50 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _putchar - writes result to the console
+ * @c: input character
+ * Return: prints result to console
+ */
+int _putchar (char c)
+{
+	return (write(1,c,1));
+}
