351f3ff9f218771d75965f3d3103427d9dbf470c
solkiona <solkiona@gmail.com>
2023-02-24 21:42:27 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int ch;
 
-	for (i = 48; i <= 57; i++)
+	for (ch = 48; ch < 58; ch++)
 	{
-		if (i != 50 && i != 52)
-			_putchar(i);
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
 	}
 	_putchar('\n');
 }
