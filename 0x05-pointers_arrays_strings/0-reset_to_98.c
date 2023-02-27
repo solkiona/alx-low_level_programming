79c8a404ef697ea98be28cb4ae2cda6366690159
solkiona <solkiona@gmail.com>
2023-02-27 08:09:48 -0800

    array to pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - returns the value of the pointer integer to 98
+ * @n: initial integer value
+ * Return: no return value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
