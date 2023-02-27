fb8986aa4f399bdaf4383e42cccccd8b9b621934
solkiona <solkiona@gmail.com>
2023-02-27 08:30:26 -0800

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * void swap_int - writes a function that swaps the values of two integers
+ * @a: first input
+ * @b: second input
+ * Return: no return value
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
