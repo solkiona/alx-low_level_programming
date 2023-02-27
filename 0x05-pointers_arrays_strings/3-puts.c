75796c2c82bd68776c724587cadc967ef568b664
solkiona <solkiona@gmail.com>
2023-02-27 09:38:21 -0800

    implement puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - writes a function that prints a string
+ * @str: input string
+ * Return: No return value
+ */
+void _puts(char *str)
+{
+	int i=0;
+
+	while (str[i] >= 0)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[i]);
+		i++;
+	}
+}
