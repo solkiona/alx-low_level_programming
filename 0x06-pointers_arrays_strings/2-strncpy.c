378ccb325dc6bed0e22f3fd8f93f1e2674e0fa22
solkiona <solkiona@gmail.com>
2023-03-02 07:46:51 -0800

    strnccpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
-for (i = 0; i < n && *src != '\0'; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
-*dest++ = *src++;
+dest[i] = src[i];
 }
 for (; i < n; i++)
 {
-*dest++ = '\0';
+dest[i] = '\0';
 }
 return (dest);
 }
