82777d6d6a38c68a9fdf3904008e39fea484314d
solkiona <solkiona@gmail.com>
2023-03-02 14:22:20 -0800

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,21 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	int i, op;
+
+	while (op == 0)
 	{
-		s1++;
-		s2++;
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		op = s1[i] - s2[i];
+		i++;
 	}
-	if (*s1 == *s2)
-	{
-		return (0);
-	}
-	else if (*s1 < *s2)
-	{
-		return (-1);
-	}
-	else
-	{
-		return (1);
-	}
+	return (op);
 }
