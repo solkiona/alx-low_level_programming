13bd584d777534f680fb982d363043e8c70521dd
solkiona <solkiona@gmail.com>
2023-03-06 18:34:32 -0800

    prefix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 #include <stddef.h>
 unsigned int _strspn(char *s, char *accept);
+#include <string.h>
 
 #endif /* MAIN_H */
