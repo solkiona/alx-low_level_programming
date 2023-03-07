b8d33bebbbe0a1e5d9b8c23d2fe13d9bf4bc0939
solkiona <solkiona@gmail.com>
2023-03-06 20:28:54 -0800

    change the value of a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - writes a function that sets the value of apointer
+ * @s: Input string
+ * @to: new string
+ * Return: no return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
