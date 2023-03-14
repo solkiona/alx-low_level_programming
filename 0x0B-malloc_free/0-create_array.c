4ed39eac0c2753a977fbbbbdbf25161bab0986a7
solkiona <solkiona@gmail.com>
2023-03-14 09:11:31 -0700

    malloco

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,13 +9,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+
 	if (size == 0)
 		return (NULL);
-	char *arr = (char *)malloc(size * sizeof(char));
+	arr = (char *)malloc(size * sizeof(char));
 
 	if (arr == NULL)
 		return (NULL);
-	for (unsigned int i = 0; i < size; i++)
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
 		arr[i] = c;
 	return (arr);
 }
