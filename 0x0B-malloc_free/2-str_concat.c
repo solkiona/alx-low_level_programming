24adba823739428afdfa7096bb9d45e05c45f103
solkiona <solkiona@gmail.com>
2023-03-14 21:16:12 -0700

    string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	char *result;
 	unsigned int i;
 
-	while (*s1 != '\0')
+	while (s1[len1] != '\0')
 		len1++;
-	while (*s2 != '\0')
+	while (s2[len2] != '\0')
 		len2++;
 	if (s1 == NULL)
 		s1 = "";
@@ -27,7 +27,8 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
-	result[len1 + i] = s2[i];
+	for (i = 0; i < len2; i++)
+		result[len1 + i] = s2[i];
 	result[len1 + len2] = '\0';
 	return (result);
 
