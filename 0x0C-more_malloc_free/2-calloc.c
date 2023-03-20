2860ece71e3c4233b6246a0498055dee1decd898
solkiona <solkiona@gmail.com>
2023-03-20 10:28:14 -0700

    implementing the calloc using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,10 +12,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	result = malloc(nmemb * size);
+
 	if (result == NULL)
 		return (NULL);
+
 	for (unsigned int i = 0; i < nmemb * size; ++i)
 		result[i] = 0;
+
 	return (result);
 }
