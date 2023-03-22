2a485224457e01e06a6e8f025efa79b26bf871cf
solkiona <solkiona@gmail.com>
2023-03-22 12:49:59 -0700

    This function prints a name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#define MAIN_H
-#ifndef MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
-#endif /* MAIN_H */
+#endif /* FUNCTION_POINTERS_H */
