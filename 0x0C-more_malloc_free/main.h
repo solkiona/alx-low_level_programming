a87efa029c2c233a25946ddd6bbb49ad85e40acd
solkiona <solkiona@gmail.com>
2023-03-22 13:41:14 -0700

    this program multiplies two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,4 +8,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _is_zero(char *argv[]);
+int _checknum(char *argv[], int n);
+char *_initialize_array(char *ar, int lar);
+int main(int argc, char *argv[]);
 #endif /* MAIN_H */
