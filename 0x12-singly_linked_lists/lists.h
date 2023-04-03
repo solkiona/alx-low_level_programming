95dc54a8f4bbb903a1af5035bb15131cd9fd9bfb
solkiona <solkiona@gmail.com>
2023-04-03 09:37:18 -0700

    This function prints all the elements of alist

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,10 @@
-#ifndef LIST_H
-#define LIST_H
-#include <stdlib.h>
+#ifndef _LISTS_
+#define _LISTS_
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,13 +12,19 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-size_t print_list(const list_t *h);
 
-#endif /* LIST_H */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
