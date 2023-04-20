770534aa048b865d9873ce3daed28e76b181c4f4
Adule Solomon Onwukwe <solkiona@gmail.com>
2023-04-20 14:09:57 -0700

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,13 +17,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d_name = name;
+	d_name = _strcpy(name);
 	if (d_name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	d_owner = owner;
+	d_owner = _strcpy(owner);
 	if (d_owner == NULL)
 	{
 		free(d_name);
@@ -35,4 +35,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->owner = owner;
 
 	return (d);
+}
+
+/**
+ * _strcpy - copies a source string to a destination
+ * @src: input source string
+ * Return: Returns a pointer to the string copied
+ */
+char *_strcpy(char *src)
+{
+	int len = 0;
+	int j = 0;
+	char *dest;
+
+	while (src[len] != '\0')
+		len++;
+	dest = malloc(sizeof(char) * len + 1);
+	if (dest == NULL)
+		return (NULL);
+
+	for (j = 0; src[j]; j++)
+		dest[j] = src[j];
+	dest[j] = '\0';
+
+	return (dest);
 }
