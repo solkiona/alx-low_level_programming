88889c2284d1f963a04a9dee8585d615bba5a5bb
Adule Solomon Onwukwe <solkiona@gmail.com>
2023-04-20 05:31:56 -0700

    defines a new struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Defines a dogs attributes
+ * @name: A string for the dogs name
+ * @age: An integer for the dogs age
+ * @owner: A string for the owners name
+ */
+
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-}
+} dog;
 
 #endif /* DOG_H */
