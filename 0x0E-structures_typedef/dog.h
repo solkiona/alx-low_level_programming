ac4c347fd2d569059ca342f25937a60142f19f8f
Adule Solomon Onwukwe <solkiona@gmail.com>
2023-04-20 14:13:09 -0700

    writes a function that creates a new struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,5 @@ char *owner;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
+char *_strcpy(char *src);
 #endif /* DOG_H */
