462b6027e17397ed5b3c765c6c11d6f7d5f6e4ea
Adule Solomon Onwukwe <solkiona@gmail.com>
2023-05-13 08:27:30 -0700

    Bit manipulation task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' || b[i] != '1')
 			return (0);
-		result <<= 1 | b[i] - '0';
+		result = result << 1 | b[i] - '0';
 
 	}
 	return (result);
